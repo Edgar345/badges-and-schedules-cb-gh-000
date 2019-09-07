748a50fe94f60a08ae8b8584212f845c02a5cb55
Edgar V <evenegas9@ucmerced.edu>
2019-09-07 21:57:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,6 +16,6 @@ def assign_rooms speakers
 end
 
 def printer attendees
-  batch_badge_creator(attendees).each {|creator| puts creator}
-  
+  batch_badge_creator(attendees).each {|creator| puts creator }
+  assign_rooms(attendees).each { |room| puts room }
 end
